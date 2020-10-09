9924833d52da03055fdb2e9a50770ad1fe7c37f4
layzhang <layzhang@pano.im>
2020-10-09 22:55:24 +0800

    fix file module high cpu issue

--- file.go
+++ file.go
@@ -2,16 +2,15 @@ package file
 
 import (
 	"bytes"
-	"encoding/json"
-	"io/ioutil"
 	"log"
+	"io/ioutil"
 	"os"
-	"sort"
-	"strconv"
-	"strings"
 	"text/template"
 	"time"
-
+	"strconv"
+	"log"
+	"sort"
+	"strings"
 	"github.com/gliderlabs/logspout/router"
 )
 
@@ -24,104 +23,73 @@ func init() {
 	router.AdapterFactories.Register(NewFileAdapter, "file")
 }
 
-var funcs = template.FuncMap{
-	"toJSON": func(value interface{}) string {
-		bytes, err := json.Marshal(value)
-		if err != nil {
-			log.Println("error marshalling to JSON: ", err)
-			return "null"
-		}
-		return string(bytes)
-	},
-}
-
-// NewFileAdapter returns a configured raw.Adapter
+// NewRawAdapter returns a configured raw.Adapter
 func NewFileAdapter(route *router.Route) (router.LogAdapter, error) {
-	// default action is to NOT check that log file exists before writing log entry
-	// if the file is removed while logspout is running, entries will be lost since
-	// logspout will have a file pointer to a non-existent file
-
-	// setting CHECK_LOG_FILE environment variable to any value will force check that
-	// the log file exists before writing a log entry. this could induce a large
-	// performance penalty since the file is checked (stat'ed) before every write
-
-	checkLogFileExists := false
-	os.Environ()
-	if os.Getenv("CHECK_LOG_FILE") != "" {
-		checkLogFileExists = true
-	}
-
 	// default log dir
 	logdir := "/var/log/"
-
+	
 	// get 'filename' from route.Address
 	filename := "default.log"
 	if route.Address != "" {
-		filename = route.Address
+	    filename = route.Address
 	}
 	//log.Println("filename [",filename,"]")
-
-	structuredData := route.Options["structured_data"]
-
-	tmplStr := "{ \"container\" : \"{{ .Container.Name }}\", \"labels\": {{ toJSON .Container.Config.Labels }}, \"timestamp\": \"{{ .Time.Format \"2006-01-02T15:04:05Z0700\" }}\", \"source\" : \"{{ .Source }}\", \"line\": {{.Data}}\n"
-	if structuredData != "true" {
-		tmplStr = "{ \"container\" : \"{{ .Container.Name }}\", \"labels\": {{ toJSON .Container.Config.Labels }}, \"timestamp\": \"{{ .Time.Format \"2006-01-02T15:04:05Z0700\" }}\", \"source\" : \"{{ .Source }}\", \"line\": {{ toJSON .Data }} }\n"
-	}
-	tmpl, err := template.New("file").Funcs(funcs).Parse(tmplStr)
+	
+	tmplStr := "{{.Data}}\n"
+	tmpl, err := template.New("file").Parse(tmplStr)
 	if err != nil {
 		return nil, err
 	}
 
-	// default maxfilecount 10
+	// default max file count (10)
 	maxfilecount := 10
 	if route.Options["maxfilecount"] != "" {
 		maxcountStr := route.Options["maxfilecount"]
 		maxcount, err := strconv.Atoi(maxcountStr)
 		if err == nil {
-			maxfilecount = maxcount
+		    maxfilecount = maxcount
 		}
 	}
-
+	
 	// default max size (100Mb)
-	maxfilesize := 1024 * 1024 * 100
+	maxfilesize := 1024*1024*100
 	if route.Options["maxfilesize"] != "" {
 		szStr := route.Options["maxfilesize"]
 		sz, err := strconv.Atoi(szStr)
 		if err == nil {
-			maxfilesize = sz
+		    maxfilesize = sz
 		}
 	}
 	//log.Println("maxfilesize [",maxfilesize,"]")
-
+	
+	
 	a := Adapter{
-		route:        route,
-		filename:     filename,
-		logdir:       logdir,
-		maxfilesize:  maxfilesize,
+		route: route,
+		filename:  filename,
+		logdir:  logdir,
+		maxfilesize: maxfilesize,
 		maxfilecount: maxfilecount,
-		tmpl:         tmpl,
-		checklogfile: checkLogFileExists,
+		tmpl:  tmpl,
 	}
-
+	
 	// rename if exists, otherwise create it
 	err = a.Rotate()
 	if err != nil {
-		return nil, err
+	    return nil, err
 	}
 	return &a, nil
 }
 
 // Adapter is a simple adapter that streams log output to a connection without any templating
 type Adapter struct {
-	filename     string
-	logdir       string
-	filesize     int
-	maxfilesize  int
+	filename  string
+	logdir  string
+	filesize  int
+	maxfilesize   int
 	maxfilecount int
-	fp           *os.File
-	route        *router.Route
-	tmpl         *template.Template
-	checklogfile bool
+	fp  *os.File
+	route *router.Route
+	tmpl  *template.Template
 }
 
 // CheckFile makes sure file exists for writing
@@ -147,26 +115,18 @@ func (a *Adapter) Stream(logstream chan *router.Message) {
 			log.Println("err:", err)
 			return
 		}
-
-		if a.checklogfile {
-			a.CheckFile()
-		}
-
 		//log.Println("debug:", buf.String())
 		_, err = a.fp.Write(buf.Bytes())
 		if err != nil {
 			log.Println("err:", err)
 		}
-
+		
 		// update file size
-		a.filesize = a.filesize + len(buf.Bytes())
-
-		log.Println(a.filesize)
-		log.Println(a.maxfilesize)
-
-		// rotate file if size exceed max size
+		a.filesize = a.filesize+len(buf.Bytes())
+		
+		// rotate file if size exceed max size 
 		if a.filesize > a.maxfilesize {
-			a.Rotate()
+		    a.Rotate()
 		}
 	}
 }
@@ -204,35 +164,35 @@ func (a *Adapter) PruneLogs() (err error) {
 	return nil
 }
 
-// Rotate rotates log file
+// Perform the actual act of rotating and reopening file.
 func (a *Adapter) Rotate() (err error) {
 	// Close existing file if open
-	if a.fp != nil {
-		err = a.fp.Close()
-		//log.Println("Close existing file pointer")
-		a.fp = nil
-		if err != nil {
-			return err
-		}
-	}
-	// Rename dest file if it already exists
-	_, err = os.Stat(a.logdir + a.filename)
-	if err == nil {
-		err = os.Rename(a.logdir+a.filename, a.logdir+a.filename+"."+time.Now().Format(time.RFC3339))
-		log.Println("Rename existing log file")
-		if err != nil {
-			return err
-		}
-	}
-	// Create new file.
-	a.fp, err = os.Create(a.logdir + a.filename)
-	log.Println("Create new log file")
-	if err != nil {
-		return err
-	}
-	a.filesize = 0
+    if a.fp != nil {
+        err = a.fp.Close()
+        //log.Println("Close existing file pointer")
+        a.fp = nil
+        if err != nil {
+            return err
+        }
+    }
+    // Rename dest file if it already exists
+    _, err = os.Stat(a.logdir+a.filename)
+    if err == nil {
+        err = os.Rename(a.logdir+a.filename, a.logdir+a.filename+"."+time.Now().Format(time.RFC3339))
+        log.Println("Rename existing log file")
+        if err != nil {
+            return err
+        }
+    }
+    // Create new file.
+    a.fp, err = os.Create(a.logdir+a.filename)
+    log.Println("Create new log file")
+    if err != nil {
+        return err
+    }
+    a.filesize = 0
 
-	a.PruneLogs()
+    a.PruneLogs()
 
-	return nil
+    return nil
 }
